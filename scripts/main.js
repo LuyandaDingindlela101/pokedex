f5ee72259fe84b79d7adea22bccc94310208903d
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2021-07-01 22:24:29 +0200

    all functionality complete - responsiveness is still left

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+let pokemon = {};
 let base_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 // Function to fetch a list of pokemon
@@ -25,132 +26,49 @@ function getPokemonList(url) {
 	});
 }
 
-// Get default pokemon list
-getPokemonList(base_URL);
-
 // Function to get information about a specific pokemin
 function getPokemonInfo(url) {
-	let abilities_list = document.querySelector(".abilities-list");
-	let moves_list = document.querySelector(".moves-list");
-	
-	let abilities_heading = document.querySelector(".abilities-heading");
-	let moves_heading = document.querySelector(".moves-heading");
-	
+     // CALL THE openPokeball FUNCTION
 	openPokeball();
 	fetch(url)
 	.then((response) => response.json())
 	.then((data) => {
-		// Make sure data comes throufg
-		console.log(data);
+          // CREATE AND POPULATE THE pokemon OJECT WITH APPROPRIATE DATA
+          let pokemon = {
+               name: data.name,
+               height: data.height + " m",
+               weight: data.weight + " lbs",
+               type: data.types[0].type.name,
+               image_url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + data.id + ".png",
+          }
 
-		abilities_list.style.display = "flex";
-		moves_list.style.display = "flex";
-
-		abilities_list.innerHTML = "";
-		moves_list.innerHTML = "";
-
-		abilities_heading.textContent = "Abilities";
-
-
-		for (let ability_item of data.abilities) {
-			// console.log(ability_item);
-			fetch(ability_item.ability.url)
-			.then((response) => response.json())
-			.then((abilities_data) => {
-
-				// Write data to pokemon information container
-				document.querySelector(".pokeball-top").innerHTML = 
-				`
-					<h2 class="pokemon-name">${data.name}</h2>
-					<div class="pokemon-image"><img src="${data.sprites.front_default} "></div>
-				`;
-
-				for (let effect_item of abilities_data.effect_entries) {
-					if (effect_item.language.name == "en") {
-						console.log(effect_item);
-						abilities_list.innerHTML += 
-						`
-							<li>
-								<div class="abilities-description">
-									<h4>${ability_item.ability.name}</h4>
-									<p>${effect_item.effect}</p>
-								</div>
-							</li>
-						`;
-					}
-				}
-
-			});
-		}
-		
-		moves_heading.textContent = "Moves";
-
-		let index = 0;
-		for (let move_item of data.moves) {
-			// console.log(move_item.move);
-			fetch(move_item.move.url)
-			.then((response) => response.json())
-			.then((moves_data) => {
-				console.log(moves_data.flavor_text_entries);
-
-				// for (let flavour_item of moves_data.flavor_text_entries) {
-				// 	console.log(flavour_item);
-					moves_list.innerHTML += 
-					`
-						<li>${data.moves[index].move.name}</li>
-					`;
-					if (flavour_item.version_group.name == "diamond-pearl") {
-						moves_list.innerHTML += 
-						`
-							<li>${data.moves[index].move.name}</li>
-						`;
-			
-					}
-				// }
-				// for (let i = 0; i < 2; i++) {
-				// 	console.log(moves_data.flavor_text_entries[i].flavor_text);
-				// }
-
-				// Write data to pokemon information container
-				// document.querySelector(".pokeball-top").innerHTML = 
-				// `
-				// 	<h2 class="pokemon-name">${data.name}</h2>
-				// 	<div class="pokemon-image"><img src="${data.sprites.front_default} "></div>
-				// `;
-
-				// for (let effect_item of moves_data_data.effect_entries) {
-				// 	if (effect_item.language.name == "en") {
-				// 		console.log(effect_item);
-				// 		moves_data_list.innerHTML += 
-				// 		`
-				// 			<li>
-				// 				<div class="moves_data-description">
-				// 					<h4>${move_item.ability.name}</h4>
-				// 					<p>${effect_item.effect}</p>
-				// 				</div>
-				// 			</li>
-				// 		`;
-				// 	}
-				// }
-			});
-
-			index++;
-			
-			if (index == 1) return;
-		}
-				
-				
-		for (let index in data.moves) {
-			moves_list.innerHTML += 
-			`
-				<li>${data.moves[index].move.name}</li>
-			`;
-
-			if (index == 1) return;
-		}
+          // UPDATE THE HTML WITH THE CORRECT INFO
+          document.querySelector(".pokeball-image").innerHTML = `<img src="${pokemon.image_url}" alt="">`
+          document.querySelector(".pokeball-details h1").textContent = `${pokemon.name}`
+          document.querySelector(".stats-container").innerHTML = 
+          `
+               <div class="type-container">
+                    <span>${pokemon.type}</span>
+                    <p>Type</p>
+               </div>
+               <div class="weight-container">
+                    <span>${pokemon.weight}</span>
+                    <p>Weight</p>
+               </div>
+               <div class="height-container">
+                    <span>${pokemon.height}</span>
+                    <p>Height</p>
+               </div>
+          `
 	});
 }
 
+// FUNCTION WILL ADD THE ACTIVE CLASS TO THE .pokeball ELEMENT
 function openPokeball() {
 	document.querySelector(".pokeball").classList.add("active");
 }
+
+
+// Get default pokemon list
+getPokemonList(base_URL);
+
